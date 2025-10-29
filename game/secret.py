9b6e7d440396e8f2d39410d5254036f556749b27
Or200 <avimeda74@gmail.com>
2025-10-29 10:47:07 +0200

    add generate secret func

--- game/secret.py
+++ game/secret.py
@@ -1,4 +1,29 @@
 import random
 
-def generate_secret(length: int = 4, *, unique_digits: bool = True, allow_leading_zero: bool = False, rng: random.Random | None = None) -> str:
-    pass
+def generate_secret(length: int = 4, *, unique_digits: bool = True, allow_leading_zero: bool = False) -> str:
+    secret_num_list = []
+    secret_num_set = set()
+
+    if unique_digits:
+        while len(secret_num_set) != length:
+            num = int((random.random()) // 0.1)
+            secret_num_set.add(str(num))
+
+        secret_num_list = list(secret_num_set)
+
+    else:
+        while len(secret_num_list) != length:
+            num = int((random.random()) // 0.1)
+            if num not in secret_num_list:
+                secret_num_list.append(str(num))
+
+
+
+    if not allow_leading_zero:
+        while secret_num_list[0] == "0":
+            secret_num_list[0] = str(int((random.random()) // 0.1))
+        
+
+
+    secret_num = "-".join(secret_num_list)
+    return secret_num
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from game.secret import generate_secret
+
 def play(length: int = 4, max_tries: int | None = 12, *, unique_digits: bool = True, allow_leading_zero: bool = False) -> None:
-    pass
+    generate_secret()
+
 
 
 if __name__ == "__main__":
