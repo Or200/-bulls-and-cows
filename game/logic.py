157077d08ccc5a739d4668ef76375dc65fe5b15d
Or200 <avimeda74@gmail.com>
2025-10-29 12:00:43 +0200

    add init state func

--- game/logic.py
+++ game/logic.py
@@ -20,20 +20,20 @@ def score_guess(secret: str, guess: str) -> tuple[int, int]:
 def is_won(bulls: int, length: int) -> bool:
     return bulls == length
 
-# class GameState(TypedDict): {
-#     "secret": str,
-#     "length": int,
-#     "max_tries": int | None,
-#     "tries_used": int,
-#     "unique_digits": bool,
-#     "allow_leading_zero": bool,
-#     "history": list[tuple[str, int, int]], # [(guess, bulls, cows), ...]
-#     "seen": set[str] # ניחושים שנעשו
-# }
     
-def init_state(secret: str, length: int, max_tries: int | None, unique_digits: bool, allow_leading_zero: bool) -> GameState:
-    pass
+def init_state(secret: str, length: int, max_tries: int | None, unique_digits: bool, allow_leading_zero: bool, seen: dict) -> dict:
+    GameState = {
+    "secret": secret,
+    "length": length,
+    "max_tries": max_tries,
+    "tries_used": 0,
+    "unique_digits": unique_digits,
+    "allow_leading_zero": allow_leading_zero,
+    "history": [],
+    "seen": seen
+}
 
-def apply_guess(state: GameState, guess: str) -> tuple[int, int]:
+
+def apply_guess(state: dict, guess: str) -> tuple[int, int]:
     pass
 
